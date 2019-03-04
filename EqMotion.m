c2f61e77cf28ce2498b553b61029ee511373cbeb
Will-Schjeld <william.schjelderup@gmail.com>
2019-03-03 17:55:14 -0800

    Updated Variable Names

--- EqMotion.m
+++ EqMotion.m
@@ -4,14 +4,14 @@ function xdot = EqMotion(t,x)
 	global CL CD S m g rho
 	
 	V 	=	x(1);
-	Gam	=	x(2);
+	Gamma	=	x(2);
 	q	=	0.5 * rho * V^2;	% Dynamic Pressure, N/m^2
     
     
 	%a = (drag force + force of gravity)/m
     %Vx or Vy = Vsin(gam)
-	xdot	=	[(-CD * q * S - m * g * sin(Gam)) / m
-				 (CL * q * S - m * g * cos(Gam)) / (m * V)
-				 V * sin(Gam)
-				 V * cos(Gam)];
+	xdot	=	[(-CD * q * S - m * g * sin(Gamma)) / m
+				 (CL * q * S - m * g * cos(Gamma)) / (m * V)
+				 V * sin(Gamma)
+				 V * cos(Gamma)];
              
--- Project.m
+++ Project.m
@@ -2,9 +2,9 @@
 %	Copyright 2005 by Robert Stengel
 %	August 23, 2005
 
-	global CL CD S m g rho	
-	S		=	0.017;			% Reference Area, m^2
-	AR		=	0.86;			% Wing Aspect Ratio
+	global CL CD A m g rho	
+	A		=	0.017;			% Reference Area, m^2
+	AR		=	0.86;			% Wing Aspect Ratio. Wingspan / Chord
 	e		=	0.9;			% Oswald Efficiency Factor;
 	m		=	0.003;			% Mass, kg
 	g		=	9.8;			% Gravitational acceleration, m/s^2
@@ -16,22 +16,22 @@
 	CL		=	sqrt(CDo / epsilon);	% CL for Maximum Lift/Drag Ratio
 	CD		=	CDo + epsilon * CL^2;	% Corresponding CD
 	LDmax	=	CL / CD;			% Maximum Lift/Drag Ratio
-	Gam		=	-atan(1 / LDmax);	% Corresponding Flight Path Angle, rad
-	V		=	sqrt(2 * m * g /(rho * S * (CL * cos(Gam) - CD * sin(Gam))));
+	Gamma		=	-atan(1 / LDmax);	% Corresponding Flight Path Angle, rad
+	V		=	sqrt(2 * m * g /(rho * A * (CL * cos(Gamma) - CD * sin(Gamma))));
 							% Corresponding Velocity, m/s
 	Alpha	=	CL / CLa;			% Corresponding Angle of Attack, rad
 	
 %	a) Equilibrium Glide at Maximum Lift/Drag Ratio
-	H		=	2;			% Initial Height, m
-	R		=	0;			% Initial Range, m
+	Y		=	2;			% Initial Height, m
+	X		=	0;			% Initial Range, m
 	to		=	0;			% Initial Time, sec
 	tf		=	6;			% Final Time, sec
 	tspan	=	[to tf];
-	xo		=	[V;Gam;H;R];
+	xo		=	[V;Gamma;Y;X];
 	[ta,xa]	=	ode23('EqMotion',tspan,xo);
 	
 %	b) Oscillating Glide due to Zero Initial Flight Path Angle
-	xo		=	[V;0;H;R];
+	xo		=	[V;0;Y;X];
 	[tb,xb]	=	ode23('EqMotion',tspan,xo);
 	
 	figure
