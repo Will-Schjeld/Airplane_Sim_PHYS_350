9e05fdb48a1607b8e6a20abc107a4875d9f1a67c
Will-Schjeld <william.schjelderup@gmail.com>
2019-03-18 00:31:13 -0700

    Added 3D equations. Needs Constants and Improvement

--- EqMotion.m
+++ EqMotion.m
@@ -1,16 +1,57 @@
-function xdot = EqMotion(t,x)
-%	Fourth-Order Equations of Aircraft Motion
+function eqns = EqMotion(t,x)
 
-	global CL CD A m g rho
-	
-	V 	=	x(1);
-	Gamma	=	x(2);
-	q	=	0.5 * rho * V^2;	% Dynamic Pressure, N/m^2
+	global Ixx Iyy Izz Ixz A m g rho c b
     
+    u = x(1);
+    v = x(2);
+    w = x(3);
+    p = x(4);
+    q = x(5);
+    r = x(6);
+    phi = x(10);
+    theta = x(11);
+    psi = x(12);
     
-	%a = (drag force + force of gravity)/m
-    %Vx or Vy = Vsin(gam)
-	xdot	=	[(-CD * q * A - m * g * sin(Gamma)) / m
-				 (CL * q * A - m * g * cos(Gamma)) / (m * V)
-				 V * sin(Gamma)
-				 V * cos(Gamma)];
+    Va = sqrt(u^2 + v^2 + w^2);
+    %beta = asin(v / Va);
+    alpha = atan(w / u);
+    qbar  =	0.5 * rho * Va^2;	% Dynamic Pressure, N/m^2
+    gx = -g*sin(theta);
+    gy = g*sin(phi)*cos(theta);
+    gz = g*cos(phi)*cos(theta);
+    
+    Cx = 1;
+    Cy = 1;
+    Cz = 1;
+    Cn = 2*sin(alpha)^2; %eqn 2.4-3
+    Cl = Cn*cos(alpha);  %eqn 2.4-4a look at 4b for a better approximation
+    Cm = 1; %eqn 2.4-43
+    
+    X = Cx * qbar * A;
+    Y = Cy * qbar * A;
+    Z = Cz * qbar * A;
+    L = Cl * qbar * A * b;
+    M = Cm * qbar * A * c;
+    N = Cn * qbar * A * b;
+    
+    %Cartesian Accelerations
+    udot   = X/m + gx + r*v - q*w;
+	vdot   = Y/m + gy - r*u + p*w;
+    wdot   = Z/m + gz + q*u - p*v;
+    
+    %Roll, Pitch, Yaw Accelerations (Angular Accelerations)
+    pdot   = (Ixx*L + Ixz*N - (Ixz*(Iyy - Ixx - Izz) * p + (Ixz^2 + Izz*(Izz - Iyy))*r)*q) / (Ixx*Izz - Ixz^2);
+    qdot   = (M - (Ixx - Izz)*p*r - Ixz*(p^2-r^2)) / Iyy;
+    rdot   = (Ixz*L + Ixx*N + (Ixz*(Iyy - Ixx - Izz) * r + (Ixz^2 + Ixx*(Ixx - Iyy))*p)*q) / (Ixx*Izz - Ixz^2);
+    
+    %Standard Cartesian Cord Velocities
+    xdot   = (cos(theta)*cos(psi))*u + (-cos(phi)*sin(psi) + sin(phi)*sin(theta)*cos(psi))*v + (sin(phi)*sin(psi) + cos(phi)*sin(theta)*cos(psi))*w;
+    ydot   = (cos(theta)*sin(psi))*u + (cos(phi)*cos(psi) + sin(phi)*sin(theta)*sin(psi))*v + (-sin(phi)*cos(psi) + cos(phi)*sin(theta)*sin(psi))*w;
+    zdot   = (-sin(theta)*u + (sin(phi)*cos(theta))*v + (cos(phi)*cos(theta)*w));
+    
+    %Euler Angles Rate of Change
+    phidot = p + (q*sin(phi) - r*cos(phi))*tan(theta);
+    thetadot = q*cos(phi) - r*sin(phi);
+    psidot = (q*sin(phi) + r*cos(phi))*sec(theta);
+    
+	eqns	=	[udot vdot wdot pdot qdot rdot xdot ydot zdot phidot thetadot psidot]';
--- Project.m
+++ Project.m
@@ -1,13 +1,21 @@
-%	Example 1.3-1 Paper Airplane Flight Path
-%	Copyright 2005 by Robert Stengel
-%	August 23, 2005
-
-	global CL CD A m g rho	
+%   PHYS 350 Project
+%	March 17, 2019
+    clc;
+    clear;
+    
+	global Ixx Iyy Izz Ixz A m g rho c b
+    Ixx     =   .5;              %TODO: CALCULATE
+    Iyy     =   .5;
+    Izz     =   .2;
+    Ixz     =   .1;
+    c       =   .2;             % chord length, m, complete guess ****************
+    b       =   .1;              % ?????
+    
 	A		=	0.017;			% Reference Area, m^2
 	AR		=	0.86;			% Wing Aspect Ratio. Wingspan / Chord
 	e		=	0.9;			% Oswald Efficiency Factor;
 	m		=	0.003;			% Mass, kg
-	g		=	9.8;			% Gravitational acceleration, m/s^2
+	g		=	9.81;			% Gravitational acceleration, m/s^2
 	rho		=	1.225;			% Air density at Sea Level, kg/m^3	
 	CLa		=	pi * AR/(1 + sqrt(1 + (AR / 2)^2));
 							% Lift-Coefficient Slope, per rad
@@ -22,33 +30,33 @@
 	Alpha	=	CL / CLa;			% Corresponding Angle of Attack, rad
 	
 %   a) Equilibrium Glide at Maximum Lift/Drag Ratio
-	H		=	2;			% Initial Height, m
-	R		=	0;			% Initial Range, m
+	z		=	2;			% Initial Height, m
+	x		=	0;			% Initial x, m
+    y       =   0;          % Initial y, m
 	to		=	0;			% Initial Time, sec
-	tf		=	6;			% Final Time, sec
+	tf		=	1;			% Final Time, sec
 	tspan	=	[to tf];
-	xo		=	[V;Gamma;H;R];
+	xo		=	[1 0 0 0 0 0 x y z 0 0 0]';         % Given as [u v w p q r x y z phi theta psi]
 	[ta,xa]	=	ode23('EqMotion',tspan,xo);
 	
 %	b) Oscillating Glide due to Zero Initial Flight Path Angle
-	xo		=	[V;0;H;R];
-    xb      = zeros(1,4);
-	[tb,xb]	=	ode23('EqMotion',tspan,xo);
+%	xo		=	[V;0;z;x];
+%   xb      = zeros(1,4);
+%   [tb,xb]	=	ode23('EqMotion',tspan,xo);
 	
-	figure
-	plot(xa(:,4),xa(:,3),xb(:,4),xb(:,3))
-	xlabel('Range, m'), ylabel('Height, m'), grid
-
-	figure
-	subplot(2,2,1)
-	plot(ta,xa(:,1),tb,xb(:,1))
-	xlabel('Time, s'), ylabel('Velocity, m/s'), grid
-	subplot(2,2,2)
-	plot(ta,xa(:,2),tb,xb(:,2))
-	xlabel('Time, s'), ylabel('Flight Path Angle, rad'), grid
-	subplot(2,2,3)
-	plot(ta,xa(:,3),tb,xb(:,3))
-	xlabel('Time, s'), ylabel('Altitude, m'), grid
-	subplot(2,2,4)
-	plot(ta,xa(:,4),tb,xb(:,4))
-	xlabel('Time, s'), ylabel('Range, m'), grid
+    plot3(xa(:,7),xa(:,8),xa(:,9))
+%    plot(xa(:,4),xa(:,3),xb(:,4),xb(:,3))
+	xlabel('Range X, m'), ylabel('Range Y, m'), zlabel('Height, m'), grid 
+% 	figure
+% 	subplot(2,2,1)
+% 	plot(ta,xa(:,1),tb,xb(:,1))
+% 	xlabel('Time, s'), ylabel('Velocity, m/s'), grid
+% 	subplot(2,2,2)
+% 	plot(ta,xa(:,2),tb,xb(:,2))
+% 	xlabel('Time, s'), ylabel('Flight Path Angle, rad'), grid
+% 	subplot(2,2,3)
+% 	plot(ta,xa(:,3),tb,xb(:,3))
+% 	xlabel('Time, s'), ylabel('Altitude, m'), grid
+% 	subplot(2,2,4)
+% 	plot(ta,xa(:,4),tb,xb(:,4))
+% 	xlabel('Time, s'), ylabel('Range, m'), grid
