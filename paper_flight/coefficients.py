fixed units

paper_flight/coefficients.py
@@ -17,7 +17,7 @@ Cr = 186e-3        #mm    -- Root Chord
 Cb = 138.041e-3    #mm    -- Mean Aerodynamic Chord
 Ml = 140e-3        #mm    -- Mean Aerodynamic Chord Location
 CG = 81e-3         #mm    -- Center of Gravity
-SM = (59/Cr)*1e-3  #mm    -- Static Margin
+SM = 59e-3         #mm    -- Static Margin
 G = 5*np.pi/180    #rad   -- Dihedral Angle
 e = 0.9            #      -- Oswald Efficiency Factor
 zwing = -28.85e-3  #mm    -- Distance of quarter chord below centerline
@@ -25,7 +25,7 @@ k0 = 0.075         #      -- Wing Yaw First Order Coefficient
 k1 = 0.175         #      -- Wing Yaw Second Order Coefficient
 
 #environmental and material constants
-v = 15.52e-6       #mm^2/s -- Kinematic Viscosity of air @ 25 deg C
+v = 15.52e-6       #m^2/s -- Kinematic Viscosity of air @ 25 deg C
 
 #takes the angle of attack and pitch rate in the body frame
 #and returns the static aerodynamic constants [CD, CL, CY, Cl, Cm, Cn]

paper_flight/equations.py
@@ -22,18 +22,18 @@ import transformations
 #x(11)  =   Inertial yaw angle,            psi,   rad 
 
 #environmental constants @25 deg C and atmospheric pressure
-dens = 1.1839e-6   #g/mm^3  -- Density of Air
-g = -9810e-3       #mm/s^2  -- Acceleration due to gravity 
+dens = 1.1839      #kg/m^3  -- Density of Air
+g = -9810e-3       #m/s^2  -- Acceleration due to gravity 
 
 #material and geometric constants
-S = 8211.5e-6      #mm^2   -- Wing Area
-b = 139e-3         #mm     -- Wing Span
-m = 5e-3           #g      -- Plane Mass
-Cb = 138.041e-3    #mm     -- Mean Chord
-Ixx = 4984.8e-9    #g*mm^2 -- X Area moment of Inertia
-Iyy = 29856e-9     #g*mm^2 -- Y Area Moment of Inertia
-Izz = 27664e-9     #g*mm^2 -- Z Area Moment of Inertia
-Ixz = 27664e-9     #g*mm^2 -- XZ Area Moment of Inertia
+S = 8211.5e-6      #m^2   -- Wing Area
+b = 139e-3         #m     -- Wing Span
+m = 5e-3           #kg      -- Plane Mass
+Cb = 138.041e-3    #m     -- Mean Chord
+Ixx = 4984.8e-9    #kg*m^2 -- X Area moment of Inertia
+Iyy = 29856e-9     #kg*m^2 -- Y Area Moment of Inertia
+Izz = 27664e-9     #kg*m^2 -- Z Area Moment of Inertia
+Ixz = 27664e-9     #kg*m^2 -- XZ Area Moment of Inertia
 
 #return the state equations
 def xdot(t,x):
@@ -73,17 +73,17 @@ def xdot(t,x):
     N = Cn*qbar*S*b
 
     #dynamic equations of motions
-    du = float(X/m + gb[0] + x[8]*x[1] - x[7]*x[2])
-    dv = float(Y/m + gb[1] - x[8]*x[0] + x[6]*x[2])
-    dw = float(Z/m + gb[2] + x[7]*x[0] - x[6]*x[1])
-    dx = float(y[0])
-    dy = float(y[1])
-    dz = float(y[2])
-    dp = float((Izz*L + Ixz*N - (Ixz*(Iyy - Ixx - Izz)*x[6] + (Ixz**2 + Izz*(Izz - Iyy))*x[8])*x[7])/(Ixx*Izz - Ixz**2))
-    dq = float((M - (Ixx - Izz)*x[6]*x[8] - Ixz*(x[6]**2 - x[8]**2))/Iyy)
-    dr = float((Ixz*L + Ixx*N + (Ixz*(Iyy - Ixx - Izz)*x[8] + (Ixz**2 + Ixx*(Ixx - Iyy))*x[6])*x[7])/(Ixx*Izz - Ixz**2))
-    dphi = float(x[6] + (x[7]*np.sin(x[9]) + x[8]*np.cos(x[9]))*np.tan(x[10]))
-    dtheta = float(x[7]*np.cos(x[9]) - x[8]*np.sin(x[9]))
-    dpsi = float((x[7]*np.sin(x[9]) + x[8]*np.cos(x[9]))/np.cos(x[10]))
+    du =    float(X/m + gb[0] + x[8]*x[1] - x[7]*x[2])
+    dv =    float(Y/m + gb[1] - x[8]*x[0] + x[6]*x[2])
+    dw =    float(Z/m + gb[2] + x[7]*x[0] - x[6]*x[1])
+    dx =    float(y[0])
+    dy =    float(y[1])
+    dz =    float(y[2])
+    dp =    float((Izz*L + Ixz*N - (Ixz*(Iyy - Ixx - Izz)*x[6] + (Ixz**2 + Izz*(Izz - Iyy))*x[8])*x[7])/(Ixx*Izz - Ixz**2))
+    dq =    float((M - (Ixx - Izz)*x[6]*x[8] - Ixz*(x[6]**2 - x[8]**2))/Iyy)
+    dr =    float((Ixz*L + Ixx*N + (Ixz*(Iyy - Ixx - Izz)*x[8] + (Ixz**2 + Ixx*(Ixx - Iyy))*x[6])*x[7])/(Ixx*Izz - Ixz**2))
+    dphi =  float(x[6] + (x[7]*np.sin(x[9]) + x[8]*np.cos(x[9]))*np.tan(x[10]))
+    dtheta =float(x[7]*np.cos(x[9]) - x[8]*np.sin(x[9]))
+    dpsi =  float((x[7]*np.sin(x[9]) + x[8]*np.cos(x[9]))/np.cos(x[10]))
 
     return np.array([du,dv,dw,dx,dy,dz,dp,dq,dr,dphi,dtheta,dpsi])

paper_flight/motion.py
@@ -43,11 +43,11 @@ def fly(xdot, tspan, x0):
     return [x,t]
     
 #test flight
-[x,t] = fly(xdot, [0,5], np.array([3,0,0,0,0,0,0,0,0,np.pi/3,-np.pi/4,np.pi/4]))
+[x,t] = fly(xdot, [0,5], np.array([10,4,5,0,0,0,0,0,0,0,np.pi/8,0]))
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(x[0:len(t),3],x[0:len(t),4],x[0:len(t),5])
 ax.set_xlim(0,2)
 ax.set_ylim(0,2)
-ax.set_zlim(0,-1.2)
+ax.set_zlim(-1.2,0)
 plt.show()