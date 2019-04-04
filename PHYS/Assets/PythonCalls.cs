7af80f36d73427856b22b043a10f9c7d27f8c760
Jonah Gourlay <jonah.gourlay44@gmail.com>
2019-04-03 22:29:32 -0700

    added executable change to pythoncalls

--- PHYS/Assets/PythonCalls.cs
+++ PHYS/Assets/PythonCalls.cs
@@ -22,7 +22,24 @@ namespace Flight
         {
     
             // full path of python interpreter 
-            string python = @"C:\Users\danie\Anaconda3\python.exe";
+            string python_path = "python";
+            int index = 0;
+            string path = Environment.GetEnvironmentVariable("PATH");
+            string[] pathArr = path.Split(';');
+            foreach(string item in pathArr)
+            {
+                List<string> itemList = item.Split('\\').ToList();
+                if(itemList.Contains("Python37")) {
+                    index = itemList.IndexOf("Python37");
+                }
+                else if(itemList.Contains("Python37-32")) {
+                    index = itemList.IndexOf("Python37-32");
+                }
+                if(index == itemList.Count - 1) {
+                    python_path = item + "\\python.exe";
+                }
+            }
+            string python = @python_path;
          
             // python app to call 
             string myPythonApp = @"C:\Users\danie\Desktop\PaperPlane2\PHYS\Assets\motion.py";
